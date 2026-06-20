1ec0cc4217a034ddfffd4b18d08bf5923c1842f6
李志紘 <li6664246@gmail.com>
2026-06-21 00:44:50 +0800

    Add bulletin submission functionality in PHP

---
+++ 23.bulletin_add.php
@@ -0,0 +1,20 @@
+<?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "please login first";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        $sql="insert into bulletin(title, content, type, time) 
+        values('{$_POST['title']}','{$_POST['content']}', {$_POST['type']},'{$_POST['time']}')";
+        if (!mysqli_query($conn, $sql)){
+            echo "新增命令錯誤";
+        }
+        else{
+            echo "新增佈告成功，三秒鐘後回到網頁";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }
+    }
+?>
