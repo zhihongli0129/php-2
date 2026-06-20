364a6ddfb3288447e21bcc6988d2caafa83eeba9
李志紘 <li6664246@gmail.com>
2026-06-21 00:24:36 +0800

    Create user addition form in 14.user_add_form.php

---
+++ Update 14.user_add_form.php
@@ -0,0 +1,22 @@
+<html>
+    <head><title>新增使用者</title></head>
+    <body>
+<?php        
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{    
+        echo "
+            <form action=15.user_add.php method=post>
+                帳號：<input type=text name=id><br>
+                密碼：<input type=text name=pwd><p></p>
+                <input type=submit value=新增> <input type=reset value=清除>
+            </form>
+        ";
+    }
+?>
+    </body>
+</html>
